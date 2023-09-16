65c9f83eb774e5322f37f443f923e880c456dd24
jorgeluisAG <jorge.luis.alejo.gutierrez@gmail.com>
2023-09-16 18:44:26 -0400

    crear nueva persona y direccion

--- src/main/java/taller/grado/proyectofinalbackend/model/User.java
+++ src/main/java/taller/grado/proyectofinalbackend/model/User.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.sun.istack.NotNull;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
@@ -14,6 +15,7 @@ import java.util.Date;
 @Getter
 @Setter
 @Entity
+@ToString
 @Table(name = "user")
 public class User implements Serializable {
     private static final long serialVersionUID = 1L;
@@ -60,5 +62,18 @@ public class User implements Serializable {
     @PreUpdate
     protected void onUpdate() {
         updatedAt = new Date();
+    }
+
+    public User toUser(){
+        User user = new User();
+        user.setNameUser(nameUser);
+        user.setMail(mail);
+        user.setPassword(password);
+        user.setRol(rol);
+        user.setCreatedAt(createdAt);
+        user.setUpdatedAt(updatedAt);
+        user.setPerson(person);
+        user.setAddress(address);
+        return user;
     }
 }
---
+++ src/main/java/taller/grado/proyectofinalbackend/repository/PersonRepository.java
@@ -0,0 +1,8 @@
+package taller.grado.proyectofinalbackend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import taller.grado.proyectofinalbackend.model.Person;
+
+public interface PersonRepository extends JpaRepository<Person, Integer> {
+
+}
